f55a9aa397a4665f2ee29a04302284da30f8f930
Vanessa Toro <vanetoro84@gmail.com>
2018-06-04 19:12:44 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,7 +33,7 @@ class ApplicationController < Sinatra::Base
 
 	post "/login" do
 		binding.pry
-		user = user.find_by(:username =>params[:username])
+		user = User.find_by(:username =>params[:username])
 
 		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
